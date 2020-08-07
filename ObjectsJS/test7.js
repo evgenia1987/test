18d63f6dbef97042c1f6a83d59192d773aeb42dd
evgenia1987 <e.semernikova@inostudio.com>
2020-08-07 19:35:20 +0300

    калькулятор с помощью функции-конструктора new

---
+++ ObjectsJS/test7.js
@@ -0,0 +1,19 @@
+'use strict'
+function Calculator(){
+  this.read = function(){
+    this.a = +prompt ("введите первое число", '' );
+    this.b = +prompt ("введите второе число", '' );
+  };
+  this.sum = function(){
+    return this.a + this.b;
+  };
+  this.mul = function(){
+    return this.a * this.b;
+  };
+}
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
