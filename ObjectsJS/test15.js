d6e7796c1b1a77197eb657c6e1a9ad8609b9eb75
evgenia1987 <e.semernikova@inostudio.com>
2020-08-10 15:21:03 +0300

    усечение строки

---
+++ ObjectsJS/test15.js
@@ -0,0 +1,14 @@
+'use strict'
+function truncate(str, maxlength){
+    if (str.length>=maxlength){
+        return str.slice(0,maxlength - 1)+'…';
+    }
+    else 
+        return str;
+    
+};
+
+alert(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20)); 
+alert(truncate("Всем привет!", 20)); 
+
+
