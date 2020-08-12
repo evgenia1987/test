bb1a9a1dd9648c8429bf98eeaec223b733d052cf
evgenia1987 <e.semernikova@inostudio.com>
2020-08-12 13:33:24 +0300

    сортировка пользователей по возрасту

---
+++ ObjectsJS/test29.js
@@ -0,0 +1,17 @@
+'use strict'
+const vasya = { name: "Вася", age: 25 };
+const petya = { name: "Петя", age: 30 };
+const masha = { name: "Маша", age: 28 };
+
+const arr = [ vasya, petya, masha ];
+
+function sortByAge(users) {
+    users.sort(function (a,b){return a.age-b.age;});
+}
+
+sortByAge(arr);
+
+// теперь: [vasya, masha, petya]
+alert(arr[0].name); // Вася
+alert(arr[1].name); // Маша
+alert(arr[2].name); // Петя
