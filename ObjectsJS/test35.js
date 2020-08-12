c70650590e5cc08ee628365289274f285b0f6d10
evgenia1987 <e.semernikova@inostudio.com>
2020-08-12 20:03:17 +0300

    сумма свойств объекта

---
+++ ObjectsJS/test35.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function sumSalaries(salaries) {
+
+    let sum =0;
+    for(let sal of Object.values(salaries)) {
+       sum += sal; 
+    }
+
+    return sum;
+}
+
+const salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+  };
+  
+  alert( sumSalaries(salaries) ); // 650
